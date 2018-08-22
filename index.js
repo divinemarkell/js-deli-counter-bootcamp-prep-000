bbb8ef9449342d885355401876e9d564d95d1919
Markell Owens <markellowens@mac.com>
2018-08-22 19:22:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nowServing (katzDeliLine) {
     return "There is nobody waiting to be served!";
   }
 }
-function currentLine (current) {
-  
+function currentLine (line) {
+  return "The line is currently empty.";
 }
