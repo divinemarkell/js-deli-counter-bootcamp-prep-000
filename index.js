a328f79f20fd7205b4abc9d4e3b4872de43047f0
Markell Owens <markellowens@mac.com>
2018-08-22 19:18:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function takeANumber (line, name) {
   return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 function nowServing (katzDeliLine) {
-  if (!katzDeliLine.length) {
+  if (katzDeliLine.length) {
     return `Currently serving ${katzDeliLine.shift()}.`;
   } else {
     return "There is nobody waiting to be served!";
